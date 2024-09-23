a515373065966ee59bb1d1cc84638e32d91caff6
bangdory <bemsu2@gmail.com>
2024-09-23 13:57:25 +0900

    초기설계

---
+++ src/main/java/org/soomgo/soomgo_groupware/SoomgoGroupwareApplication.java
@@ -0,0 +1,13 @@
+package org.soomgo.soomgo_groupware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoomgoGroupwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoomgoGroupwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/soomgo/soomgo_groupware/SoomgoGroupwareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soomgo.soomgo_groupware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoomgoGroupwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
