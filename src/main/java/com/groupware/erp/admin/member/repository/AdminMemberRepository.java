93272e8d39a20d31024c2ac5d667358ebb29ca64
bangdory <66317661+bangdory@users.noreply.github.com>
2024-09-25 14:40:30 +0900

    Delete src directory

--- src/main/java/com/groupware/erp/admin/member/repository/AdminMemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.groupware.erp.admin.member.repository;
-
-import com.groupware.erp.admin.member.entity.AdminMemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminMemberRepository extends JpaRepository<AdminMemberEntity, Long> {
-}
