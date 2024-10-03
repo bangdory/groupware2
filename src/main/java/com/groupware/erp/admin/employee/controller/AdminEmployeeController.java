3345ad7537d8da2d465107e24f8ae5d8e764ac74
legounder <k.eee1226@gmail.com>
2024-10-03 20:17:33 +0900

    member > employee

--- src/main/java/com/groupware/erp/admin/employee/controller/AdminEmployeeController.java
+++ src/main/java/com/groupware/erp/admin/employee/controller/AdminEmployeeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller()
-@RequestMapping(value = "/admin/member")
+@RequestMapping(value = "/admin/employee")
 public class AdminEmployeeController {
 
     @Autowired
